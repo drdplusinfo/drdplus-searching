9557c554bea5e2d306429a423187149f13ff6b33
Jaroslav Týc <mail@jaroslavtyc.com>
2018-07-19 18:49:36 +0200

    Stable versions of libraries used

--- DrdPlus/Searching/Exceptions/NonAutomaticSearchCanNotBeMadeWhenDoingFullConcentrationActivity.php
+++ DrdPlus/Searching/Exceptions/NonAutomaticSearchCanNotBeMadeWhenDoingFullConcentrationActivity.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types = 1);
+
 namespace DrdPlus\Searching\Exceptions;
 
 class NonAutomaticSearchCanNotBeMadeWhenDoingFullConcentrationActivity extends \LogicException implements Logic
